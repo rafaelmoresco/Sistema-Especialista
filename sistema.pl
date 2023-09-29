2d9c17fb45adef922afc9b1c9e955f5c3be1b3d4
Moresco <rafaelmorescovieira@gmail.com>
2023-09-29 19:03:50 -0300

    comentários arrumados

--- sistema.pl
+++ sistema.pl
@@ -79,87 +79,114 @@ prato(legumes).
 %   Regra 1
 %     SE tipo prato = Peixes e frutos do Mar
 %     E  Tipo Peixe = Peixe no vapor
-%     ENTÃO cor vinho = Branco CNF 100%
-%           tipo vinho = Seco CNF 100%
-%           corpo do vinho = leve CNF 100%
+%     ENTÃO docura hidromel = Seco
+%           corpo do hidromel = Leve
 sugestao_hidromel(Hidromel, peixes, vapor) :-  melhor_hidromel(Hidromel_cod, leve , _ , _ , seco, _ ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 2
 %     SE tipo prato = Peixes e frutos do Mar
-%     E  Tipo Peixe = Peixe fritos ou grelhado
-%     ENTÃO aroma vinho = frutado CNF 100%
-%           cor vinho = Rosé CNF 100%
-%           acidez do vinho = pouco ácido CNF 100%
+%     E  Tipo Peixe = Peixe fritos ou assado
+%     ENTÃO aroma hidromel = especiarias
+%           corpo do hidromel = Medio Encorpado
 sugestao_hidromel(Hidromel, peixes, frito_assado) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , especiarias , _ , _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 3
 %     SE tipo prato = Peixes e frutos do Mar
 %     E  Tipo Peixe = Peixe defumado
-%     ENTÃO tipo Hidromel = Seco CNF 100%
-%           cor Hidromel = Branco CNF 100%
-%           acidez do Hidromel = ácido  CNF 100%
+%     ENTÃO aroma hidromel = natural
+%           corpo do hidromel = Medio Encorpado
 sugestao_hidromel(Hidromel, peixes, defumado) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , natural , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 4
 %     SE tipo prato = Legumes
-%     E  Tipo Legume = Acompanha molho delicado
-%     ENTÃO cor Hidromel = Branco CNF 100%
-%           aroma Hidromel = frutado CNF 100%
-%           acidez do Hidromel = pouco ácido CNF 100%
+%     E  Tipo Legume = Salada seca
+%     ENTÃO docura hidromel = semiseco
+%           corpo do hidromel = Medio Encorpado
 sugestao_hidromel(Hidromel, legumes, saladas_secas) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _, semiseco , _  ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 5
 %     SE tipo prato = Legumes
-%     E  Tipo Legume = beringela ou abobrinha
-%     ENTÃO cor Hidromel = Tinto CNF 100%
-%           tipo Hidromel = Seco CNF 100%
-%           corpo do Hidromel = encorpado CNF 100%
+%     E  Tipo Legume = Salada com molho
+%     ENTÃO Docura Hidromel = Seco
+%           corpo do Hidromel = medio encorpado
 sugestao_hidromel(Hidromel, legumes, saladas_molho) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _ , seco , _ ), nome_tipo(Hidromel_cod, Hidromel).
+
+%   Regra 5
+%     SE tipo prato = Legumes
+%     E  Tipo Legume = Frutas
+%     ENTÃO  corpo do Hidromel = leve
 
 sugestao_hidromel(Hidromel, legumes, frutas) :-  melhor_hidromel(Hidromel_cod, leve , _ , _ , _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
-%   Regra 9
+%   Regra 6
 %     SE tipo prato = Carne branca
-%     E  Tipo Carne Branca = Porco
-%     OU Tipo Carne Branca = Aves
-%     ENTÃO cor Hidromel = Branco CNF 60%
-%           cor Hidromel = Tinto CNF 100%
-%           acidez do Hidromel = médio ácido CNF 100%
+%     E  Tipo Carne Branca = assada
+%     ENTÃO corpo Hidromel = medio encorpado
+%           docura Hidromel - seco
 sugestao_hidromel(Hidromel, carne_branca, assada) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _ , seco , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
-
+%   Regra 7
+%     SE tipo prato = Carne branca
+%     E  Tipo Carne Branca = frita ou com molho
+%     ENTÃO corpo Hidromel = medio encorpado
+%           docura Hidromel = semiseco
 sugestao_hidromel(Hidromel, carne_branca, frita_molho) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _ , semiseco , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
+%   Regra 8
+%     SE tipo prato = Carne intermediaria
+%     E  Tipo Carne Intermediaria = porco
+%     ENTÃO corpo Hidromel = medio encorpado
+%           docura Hidromel = semiseco
 sugestao_hidromel(Hidromel, carne_intermediaria, porco) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _ , semiseco , _ ), nome_tipo(Hidromel_cod, Hidromel).
-sugestao_hidromel(Hidromel, carne_intermediaria, porco_agridoce) :-  melhor_hidromel(Hidromel_cod, encorpado , frutado , _ , semiseco , _ ), nome_tipo(Hidromel_cod, Hidromel).
-sugestao_hidromel(Hidromel, carne_intermediaria, cervo) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _ , seco , _ ), nome_tipo(Hidromel_cod, Hidromel).
+
+%   Regra 9
+%     SE tipo prato = Carne intermediaria
+%     E  Tipo Carne Intermediaria = porco agridoce
+%     ENTÃO corpo Hidromel = encorpado
+%           aroma Hidromel = frutado
+sugestao_hidromel(Hidromel, carne_intermediaria, porco_agridoce) :-  melhor_hidromel(Hidromel_cod, encorpado , frutado , _ , _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 10
-%     SE tipo prato = Carne vermelha
-%     E  Tipo Carne Vermelha = grellhada
-%     ENTÃO cor Hidromel = Tinto CNF 100%
-%           acidez do Hidromel = médio ácido CNF 100%
-sugestao_hidromel(Hidromel, carne_vermelha, grelhada) :-  melhor_hidromel(Hidromel_cod, encorpado , frutado , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
-sugestao_hidromel(Hidromel, carne_vermelha, grelhada) :-  melhor_hidromel(Hidromel_cod, encorpado , natural , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
+%     SE tipo prato = Carne intermediaria
+%     E  Tipo Carne Intermediaria = cervo
+%     ENTÃO corpo Hidromel = medio encorpado
+%           docura Hidromel = seco
+sugestao_hidromel(Hidromel, carne_intermediaria, cervo) :-  melhor_hidromel(Hidromel_cod, medio_encorpado , _ , _ , seco , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 11
 %     SE tipo prato = Carne vermelha
-%     E  Tipo Carne Vermelha = com molho
-%     OU Tipo Carne Vermelha = assada
-%     ENTÃO tipo Hidromel = Seco CNF 100%
-%           cor Hidromel = Tinto CNF 100%
-%           corpo do Hidromel = encorpado CNF 100%
-sugestao_hidromel(Hidromel, carne_vermelha, com_molho) :- melhor_hidromel(Hidromel_cod, encorpado , natural , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
-sugestao_hidromel(Hidromel, carne_vermelha, assada) :- melhor_hidromel(Hidromel_cod, encorpado , frutado , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
+%     E  Tipo Carne Vermelha = grellhada
+%     ENTÃO corpo Hidromel = Encorpado
+%           aroma do Hidromel = frutado
+%           aroma do Hidromel = natural
+sugestao_hidromel(Hidromel, carne_vermelha, grelhada) :-  melhor_hidromel(Hidromel_cod, encorpado , frutado , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
+sugestao_hidromel(Hidromel, carne_vermelha, grelhada) :-  melhor_hidromel(Hidromel_cod, encorpado , natural , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
 %   Regra 12
+%     SE tipo prato = Carne vermelha
+%     E  Tipo Carne Vermelha = com molho
+%     ENTÃO corpo Hidromel = Encorpado
+%           aroma do Hidromel = natural
+sugestao_hidromel(Hidromel, carne_vermelha, com_molho) :- melhor_hidromel(Hidromel_cod, encorpado , natural , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
+
+%   Regra 13
+%     SE tipo prato = Carne vermelha
+%     E  Tipo Carne Vermelha = assada
+%     ENTÃO corpo Hidromel = Encorpado
+%           aroma do Hidromel = frutado
+sugestao_hidromel(Hidromel, carne_vermelha, assada) :- melhor_hidromel(Hidromel_cod, encorpado , frutado , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
+
+%   Regra 14
 %     SE tipo prato = Sobremesas
-%     E  Tipo Sobremesa = Frutas
-%     ENTÃO cor Hidromel = Branco CNF 100%
-%           cor Hidromel = Rosé CNF 80%
-%           aroma Hidromel = frutado CNF 100%
+%     E  Tipo Sobremesa = Quente
+%     ENTÃO corpo Hidromel = Encorpado
+%           aroma Hidromel = natural
 sugestao_hidromel(Hidromel, sobremesa, quente) :-  melhor_hidromel(Hidromel_cod, encorpado , natural , _, _ , _ ), nome_tipo(Hidromel_cod, Hidromel).
 
+%   Regra 14
+%     SE tipo prato = Sobremesas
+%     E  Tipo Sobremesa = Fria
+%     ENTÃO corpo Hidromel = Leve
 sugestao_hidromel(Hidromel, sobremesa, fria) :-  melhor_hidromel(Hidromel_cod, leve , _ , _, _ , _  ), nome_tipo(Hidromel_cod, Hidromel).
 
 
@@ -176,23 +203,29 @@ melhor_hidromel(melomel, Corpo, Aroma, Carbonacao, Docura, Teor) :- (Corpo = enc
 
 melhor_hidromel(metheglin, Corpo, Aroma, Carbonacao, Docura, Teor) :- (Corpo = medio_encorpado), (Aroma = especiarias), (Carbonacao = still), (Docura = seco), (Teor = great),!.
 
+%%%%%%%%%%%%%% Sobre o Sistema %%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%                                      %%    
-%%    %%   %%    %%%%%%%%    %%%%%%%    %%
-%%    %%   %%    %%          %%   %%    %%
-%%    %%   %%    %%%%%%%%    %%   %%    %%
-%%    %%   %%          %%    %%   %%    %%
-%%    %%%%%%%    %%%%%%%%    %%%%%%%    %%
-%%                                      %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Este é um sistema para recomendar um tipo de hidromel para comer com sua refeição.
+% Devido a pouca literatura sobre o assunto e o consumo de hidromel ser pouco difundido,
+% esse sistema especialista foi feito com, além das referências indicadas a seguir,
+% a experiência do integrante Rafael Moresco Vieira, produtor de hidromel artesanal como hobby.
+% Infelizmente, devido ao governo federal apenas reconhecer a produção de hidromel do estilo tradicional,
+% o acesso aos outros tipos são mais difíceis.
 
-% Para pedir uma recomendação de vinho de acordo com seu prato de comida use o comando:
-% sugestao_hidromel(Vinho, <SEU PRATO>, <SUBTIPO DO PRATO>).
+%% Referencias:
+% https://www.batchmead.com/blogs/batch/the-ultimate-guide-to-types-of-mead
+% https://www.hiddenlegendwinery.com/what-food-goes-with-mead/
+% https://www.vikingalchemist.com/mead-blog/2020/1/29/what-food-goes-well-with-mead
+% https://www.arvenhidromeis.com.br/single-post/o-guia-definitivo-de-estilos-de-hidromel
+
+%%%%%%%%%%%% Como usar o Sistema %%%%%%%%%%%%
+
+% Para pedir uma recomendação de hidromel de acordo com seu prato de comida use o comando:
+% sugestao_hidromel(hidromel, <SEU PRATO>, <SUBTIPO DO PRATO>).
 % Para receber mais de uma recomendação (se houver) aperte o botão ";" para que o programa
 % continue sugerindo. 
 
-% Sendo que SEU PRATO pode ser: carne_branca, carne_vermelha, peixe, massa, sobremesa ou legumes.
+% Sendo que SEU PRATO pode ser: carne_branca, carne_intermediaria, carne_vermelha, peixe, sobremesa ou legumes.
 % E para cada prato existe um subtipo.
 
 % Para carne_branca existe os seguintes subtipos:
@@ -223,13 +256,9 @@ melhor_hidromel(metheglin, Corpo, Aroma, Carbonacao, Docura, Teor) :- (Corpo = m
 %  - quente
 %  - fria
 
-% Para legumes existe os seguintes subtipos:
-%  - beringela_abobrinha
-%  - molho_delicado
-
 % Exemplo de execução:
-% Comando para pedir sugestão de vinho ao comer massa com molho de tomate:
+% Comando para pedir sugestão de hidromel ao comer carne de cervo:
 sugestao_hidromel(Hidromel, carne_intermediaria, cervo).
 % Output:
-%   Vinho = "Merlot" ;
+%   Hidromel = "Metheglin" ;
 %   true.
